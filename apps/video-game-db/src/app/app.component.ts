15910fdb6d76750c0d02ae8e6f493cbedce62088
Hassam <101381873+Hassam-07@users.noreply.github.com>
2024-03-05 13:14:07 +0500

    Initial commit

---
+++ apps/video-game-db/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'video-game-db-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'video-game-db';
+}
